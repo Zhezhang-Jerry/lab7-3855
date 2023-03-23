820b2ac5618ea4738ac5ad6486c4959d6a7f4e55
Zhezhang-Jerry <77636334+Zhezhang-Jerry@users.noreply.github.com>
2023-03-23 10:45:11 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -106,8 +106,7 @@ def init_scheduler():
     sched.start()
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
-CORS(app.app)
+app.add_api("openapi.yml", base_path="/processing", strict_validation=True, validate_responses=True)CORS(app.app)
 
 with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
