148cd915feb62490ad5f65ac1fd9933840ca1679
Zhezhang-Jerry <77636334+Zhezhang-Jerry@users.noreply.github.com>
2023-03-30 09:49:04 -0700

    Update create_tables.py

--- storage/create_tables.py
+++ storage/create_tables.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="172.30.0.7", user="root", password="root", database="events")
+db_conn = mysql.connector.connect(host="172.30.0.4", user="root", password="root", database="events")
 db_cursor = db_conn.cursor()
 
 db_cursor.execute('''
